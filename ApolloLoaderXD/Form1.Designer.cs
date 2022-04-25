195adfdc8963acdce2153861a3a612b085a23d29
xshonda <93495400+xshonda@users.noreply.github.com>
2022-04-24 23:08:38 -0500

    initial upload

---
+++ ApolloLoaderXD/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace ApolloLoaderXD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AllowResize = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.BorderColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(723, 450);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(12, 90, 12, 12);
+            this.ShowBorder = true;
+            this.ShowIcon = false;
+            this.ShowLeftRect = false;
+            this.ShowTitle = false;
+            this.Text = "FORM1";
+            this.TextColor = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
